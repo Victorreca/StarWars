5ee99a8a0f86e18e11b300953d8a99cf5610177c
Victor Redondo <vrviktor@gmail.com>
2025-02-17 18:58:53 +0100

    Add starshipDetail component and its path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { StarshipDetailComponent } from './components/starship-detail/starship-detail.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
+  { path: '', component: HomeComponent },
+  { path: 'starship/:id', component: StarshipDetailComponent },
 ];
---
+++ src/app/components/starship-detail/starship-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starship-detail',
+  imports: [],
+  templateUrl: './starship-detail.component.html',
+  styleUrl: './starship-detail.component.scss'
+})
+export class StarshipDetailComponent {
+
+}
