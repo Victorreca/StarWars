2dd7b5bba7faf227cc1d76a356f068bb5a778b14
Victor Redondo <vrviktor@gmail.com>
2025-02-21 20:48:12 +0100

    Move nextPageUrl  and response api to service

--- src/app/components/starships/starships.component.ts
+++ src/app/components/starships/starships.component.ts
@@ -13,30 +13,23 @@ export class StarshipsComponent implements OnInit {
   starshipsChild: Starship[] = [];
   nextPageUrl: string | null = null;
   private starshipService = inject(StarshipService);
-  urlApi: string = 'https://swapi.dev/api/starships/';
 
   ngOnInit(): void {
     this.fetchStarships();
   }
 
-  fetchStarships(url: string = this.urlApi): void {
+  fetchStarships(url: string = this.starshipService.urlApi): void {
     this.starshipService.fetchStarships(url).subscribe({
       next: (response) => {
-        this.starshipsChild = [
-          ...this.starshipsChild,
-          ...response.results.map((ship: any) => ({
-            id: ship.url.split('/').filter(Boolean).pop(),
-            name: ship.name,
-            model: ship.model,
-          })),
-        ];
-        this.nextPageUrl = response.next;
+        this.starshipsChild = [...this.starshipsChild, ...response.starships];
+        this.nextPageUrl = response.nextPageUrl;
       },
       error: (error) => {
         console.error('Error fetching ships:', error);
       },
     });
   }
+
   loadMore(): void {
     if (this.nextPageUrl) {
       this.fetchStarships(this.nextPageUrl);
--- src/app/services/starship.service.ts
+++ src/app/services/starship.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { StarshipsResponse } from '../interfaces/starships-response';
 import { Starship } from '../interfaces/starship';
 
@@ -8,10 +9,22 @@ import { Starship } from '../interfaces/starship';
   providedIn: 'root',
 })
 export class StarshipService {
+  urlApi: string = 'https://swapi.dev/api/starships/';
   constructor(private http: HttpClient) {}
 
-  fetchStarships(url: string): Observable<StarshipsResponse> {
-    return this.http.get<StarshipsResponse>(url);
+  fetchStarships(
+    url: string = this.urlApi
+  ): Observable<{ starships: Starship[]; nextPageUrl: string | null }> {
+    return this.http.get<StarshipsResponse>(url).pipe(
+      map((response) => ({
+        starships: response.results.map((ship: any) => ({
+          id: ship.url.split('/').filter(Boolean).pop(),
+          name: ship.name,
+          model: ship.model,
+        })),
+        nextPageUrl: response.next,
+      }))
+    );
   }
 
   fetchStarshipById(id: string): Observable<Starship> {
