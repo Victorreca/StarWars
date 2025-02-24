179bedf733b54fdad261b0e6d9d081c4dcbe2b18
Victor Redondo <vrviktor@gmail.com>
2025-02-24 21:52:44 +0100

    Add auth header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,26 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { StarshipDetailComponent } from './components/starship-detail/starship-detail.component';
 import { StarshipsComponent } from './components/starships/starships.component';
+import { MainLayoutComponent } from './components/layouts/main-layout/main-layout.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthLayoutComponent } from './components/layouts/auth-layout/auth-layout.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'starships', component: StarshipsComponent },
-  { path: 'starship/:id', component: StarshipDetailComponent },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'starships', component: StarshipsComponent },
+      { path: 'starship/:id', component: StarshipDetailComponent },
+    ],
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'signup', component: LoginComponent },
+      { path: 'login', component: LoginComponent },
+    ],
+  },
 ];
