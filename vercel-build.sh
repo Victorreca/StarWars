3937faad8ff999e832c4c03358699d402f7afcbf
Victor Redondo <vrviktor@gmail.com>
2025-03-10 10:54:19 +0100

    Update vercel env

---
+++ vercel-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Creando environment.ts en Vercel..."
+
+cat <<EOT > src/environments/environment.ts
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: '${API_KEY}',
+    authDomain: '${AUTH_DOMAIN}',
+    databaseURL '${DATABASE_URL}',
+    projectId: '${PROJECT_ID}',
+    storageBucket: '${STORAGE_BUCKET}',
+    messagingSenderId: '${MESSAGING_SENDER_ID}',
+    appId: '${APP_ID}'
+  }
+};
+EOT
